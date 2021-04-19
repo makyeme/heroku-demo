f84a94082cd96fdb386893a717ef7576f7c7ff23
makyeme <76942225+makyeme@users.noreply.github.com>
2021-04-19 22:04:10 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+import numpy as np
+
+
+#Initiate app
+app = Flask(__name__)
+
+
+#Load regression model
+model = pickle.load(open('linreg.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#procfile: first the ame of the pyhon file with code, then app of Flask: for this case app:app
